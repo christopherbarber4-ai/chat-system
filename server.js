611a255aaf922c49be84dfab3540de87e2823359
Chris Barber <christopherbarber4@googlemail.com>
2026-01-23 10:58:39 +0000

    added live player status

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const http = require("http");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,32 +10,46 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));});
-
-io.on("connect", (socket) => { console.log("User connected: ", socket.id);
-    
-socket.on("nameUpdate", (nameEntered) => {
-    io.emit("messageSend", ("You are now known as: " + nameEntered));
-    socket.username = nameEntered;
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
+io.on("connect", (socket) => { 
+    console.log("User connected: ", socket.id);
+    io.emit("newUser", (socket.id));
 
 
 
-socket.on("messageSend",(text) =>{
-    io.emit("messageSend", (socket.username + " says " + text));
+    socket.on("nameUpdate", (nameEntered) => {
+        io.emit("messageSend", ("You are now known as: " + nameEntered));
+        socket.username = nameEntered;
+        io.emit("newUser", (socket.username));
 
+    });
+
+    socket.on("numUpdate", (numEntered) => {
+        io.emit("messageSend", ("You rolled a " + numEntered));
+        socket.roll = numEntered;
+    });
+
+
+
+    socket.on("messageSend", (text) => {
+        io.emit("messageSend", (socket.username + " says " + text));
+
+    });
+
+
+
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: ", socket.id);
+
+        socket.emit
+    })
 });
 
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000");
 
 
-
-socket.on("disconnect", () =>{console.log ("User disconnected: ", socket.id);
-
-    socket.emit
-})});
-
-server.listen(3000, () => { console.log("Server running at http://localhost:3000");
-
-    
 })
